4d660f8962a99fa612e9d2690d859984c29aa55c
imd1lzod <dilzodbekxursanov0@gmail.com>
2025-07-10 13:29:55 +0500

    react-optimization

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const PostsPage = React.lazy(() => import("./pages/posts"));
 function App() {
   return (
     <Routes>
-      <Route path="/dashboard" element={<Main></Main>}>
+      <Route path="/" element={<Main></Main>}>
         <Route path="users" element={<UsersPage></UsersPage>}></Route>
         <Route path="products" element={<ProductsPage></ProductsPage>}></Route>
         <Route path="posts" element={<PostsPage></PostsPage>}></Route>
